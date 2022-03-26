61003b99108bcf58a24d4c74ae9a38e25551198e
Kubilay Kaya <ckubilaykaya@gmail.com>
2022-03-26 18:43:45 +0300

    login and register pages completed

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function LoadingScreen() {
+  return (
+    <div className="loading-screen">
+      <div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { TextField, CssBaseline, Button, Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <div className="container">
+      <CssBaseline />
+      <Typography variant="h5" sx={{ textAlign: "center" }}>
+        Sign in
+      </Typography>
+      <Box component="form">
+        <TextField fullWidth margin="normal" label="Email Address" required autoComplete="email" autoFocus />
+        <TextField fullWidth margin="normal" label="Password" required type="password" />
+        <Button type="submit" fullWidth sx={{ mt: 2 }} variant="contained">
+          Sign in
+        </Button>
+        <div className="d-flex justify-content-between mt-16">
+          <Link to="#">Forgot Password?</Link>
+          <Link to="/sign-up">Sign Up</Link>
+        </div>
+      </Box>
+    </div>
+  );
 }
